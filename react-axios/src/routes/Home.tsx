69de44d9994b53db1a449345d172325dd27b728b
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-03-02 21:00:49 -0300

    Update

--- react-axios/src/routes/Home.tsx
+++ react-axios/src/routes/Home.tsx
@@ -1,6 +1,6 @@
-import axios from "axios"
 import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
+import blogFetch from "../axios/config"
 
 import "./Home.css"
 
@@ -10,7 +10,7 @@ const Home = () => {
     console.log(posts)
     const getPosts = async () => {
         try {
-            const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+            const response = await blogFetch.get('/posts');
 
             const data = response.data;
 
@@ -25,7 +25,19 @@ const Home = () => {
     }, [])
 
     return (
-        <div>home</div>
+        <div className="home">
+            <h1>Ultimos posts</h1>
+            {posts.length === 0 ?
+                (<p>Carregando..</p>) :
+                (posts.map((post) => (
+                    <div className="post" key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                        <Link to={`/posts/${post.id}`} className="btn">Ler Mais</Link>
+                    </div>
+                ))
+                )}
+        </div>
     )
 }
 
--- react-axios/src/routes/NewPost.tsx
+++ react-axios/src/routes/NewPost.tsx
@@ -1,6 +1,53 @@
+import blogFetch from "../axios/config"
+
+import "./NewPost.css"
+
+import { useState } from "react"
+
+import useNavigate from "react-router-dom"
+
 
 export const NewPost = () => {
+
+    /*  const navigate = useNavigate(); */
+
+    const [title, setTitle] = useState()
+    const [body, setBody] = useState()
+
+    const createPost = async (e) => {
+        e.preventDefaut()
+        const post = { title, body, userId: 1 };
+
+        await blogFetch.post("/posts", {
+            body: post,
+        })
+    }
+
     return (
-        <div>NewPost</div>
+        <div className="new-post">
+            <h2>Inserir novo Post</h2>
+            <form onSubmit={(e) => createPost(e)}>
+                <div className="form-control">
+                    <label htmlFor="title">Título</label>
+                    <input
+                        type="text"
+                        name="title"
+                        id="title"
+                        placeholder="Digite o Titulo"
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="body">Conteúdo</label>
+                    <textarea
+                        name="body"
+                        id="body"
+                        placeholder="Digite o conteúdo"
+                        onChange={(e) => setBody(e.target.value)}
+                    ></textarea>
+                </div>
+                <input type="submit" value="Criar" className="btn" />
+            </form>
+        </div>
     )
 }
